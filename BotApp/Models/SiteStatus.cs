92145239ba6532223ca2472a794e71abf9fa2d0b
LeftGooD <leftgod@mail.ru>
2020-08-01 14:47:22 +0300

    add controls and models

---
+++ BotApp/Models/SiteStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BotApp.Models
+{
+    public class SiteStatus
+    {
+        public string URL { get; set; }
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public string ResultMessage { get; set; }
+    }
+}
