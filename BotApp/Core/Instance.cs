bee1807d6bcb79ba7ef5899e2ec201b07755e634
Константин Хетагуров <hetagurov@mwpost.ru>
2020-08-01 14:55:01 +0300

    add instanse

---
+++ BotApp/Core/Instance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSharpPlus;
+
+namespace BotApp.Core
+{
+    public class Instance
+    {
+        public DiscordClient CreateInstance()
+        {
+            return new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NjY0MDY3MjY4OTc5NzIwMTky.XhRu7w.2BCGwBZ4RLtpgrH4fpdamdkuxM8",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+        }
+    }
+}
--- BotApp/Form1.cs
+++ BotApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BotApp.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,54 @@ namespace BotApp
             InitializeComponent();
         }
 
+        public static async void SetBot()
+        {
+            var instance = new Instance().CreateInstance();
+
+            if (instance == null)
+            {
+                MessageBox.Show("Укажите токен бота в настройках");
+            }
+            else
+            {
+                try
+                {
+                    instance.MessageCreated += async e =>
+                    {
+                        var author = e.Author;
+                        //var isInIgnore = new SettingsController().GetSettingValue("IgnorableUsers")
+                        //                                         .ToUpperInvariant()
+                        //                                         .Contains(author.Username.ToUpperInvariant());
+
+                        //Не отвечаем ботам и пользователям из игнор-листа.
+                        if (!author.IsBot /*&& !isInIgnore*/)
+                        {
+                            string message = e.Message.Content;
+                            string answer = "ответ";
+                            await e.Message.RespondAsync(answer);
+                            //Logger.Log(message, answer, author, EventType.AnsweredMessage);
+                        }
+                    };
+
+                    await instance.ConnectAsync();
+                }
+                catch (Exception ex)
+                {
+                    //Logger.Log(ex);
+                }
+
+                //await instance.DisconnectAsync();
+                MessageBox.Show("Бот запущен");
+            }
+
+            //await Task.Delay(-1);
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            var t = new Task(SetBot);
+            t.Start();
         }
     }
 }
